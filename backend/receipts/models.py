6231c775e239d9b80209796972bed2ca3edd8592
adPorcar <ad.porcar@gmail.com>
2025-06-30 13:54:59 +0200

    feat: Implement receipt model

--- backend/receipts/models.py
+++ backend/receipts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Receipt(models.Model):
+    supermarket_name= models.CharField(max_length=255)
+    date = models.DateField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    product_name = models.TextField()  # maybe better class for product ?
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Receipt from {self.supermarket_name} on {self.date}"
