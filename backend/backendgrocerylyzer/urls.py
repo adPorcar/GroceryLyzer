175871362f3743e1c596afdaa438615460e71660
adPorcar <ad.porcar@gmail.com>
2025-07-01 16:39:51 +0200

    feat: Implement read and parse receipt with ocr, also list, show and delete methods.

--- backend/backendgrocerylyzer/urls.py
+++ backend/backendgrocerylyzer/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
+    path('receipts/', include('receipts.urls')),
+    #path('api/users/', include('users.urls')),
 ]
